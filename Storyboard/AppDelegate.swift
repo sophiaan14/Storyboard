ed1477df1f645e0853b8697e988aa043aa5bb0c9
SOPHIA AN <sotoky14@gmail.com>
2022-07-19 13:58:24 -0700

    i added a button

--- Storyboard/AppDelegate.swift
+++ Storyboard/AppDelegate.swift
@@ -31,6 +31,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+    
+    
 
+    @IBAction func FunFactButton(_ sender: Any) {
+        
+    }
 }
 
--- Storyboard/ViewController.swift
+++ Storyboard/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var funFactText1: UILabel!
+    @IBOutlet weak var funFactText2: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func button1(_ sender: Any) {
+        funFact1.isHidden = false
+        funFact2.isHidden = true
+        funFact3.isHidden = true
+    }
+    
+    @IBAction func button2(_ sender: Any) {
+        funFact2.isHidden = false
+        funFact1.isHidden = true
+        funFact3.isHidden = true
+    }
+    
+    @IBAction func button3(_ sender: Any) {
+        funFact3.isHidden = false
+        funFact1.isHidden = true
+        funFact2.isHidden = true
+    }
+    
+    @IBOutlet var funFact1: UILabel!
+    
+    @IBOutlet weak var funFact2: UILabel!
+    @IBOutlet weak var funFact3: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    @IBAction func button(_ sender: Any) {
+    }
 }
+    
+
 
